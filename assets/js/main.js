c3f2a79d084e5ab29900e711728e8fcf5f74b55f
Rauan Rosa <rauanoronha@hotmail.com>
2022-11-05 11:52:03 -0300

    Finalizado Consumo API, Inserção na Tela e Ajuste CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,131 @@
-var limite = 100;
-var atual = 1;
-const url = "https://pokeapi.co/api/v2/pokemon/";
 
-requisicaoAPI(url, limite, atual);
+requisicaoAPI(1, 50);
+
+function requisicaoAPI(apos, limite) {
+    let listaPokemons = [];
+
+    (limite > 898)
+        ? limite = 898
+        : limite;
+    (apos > limite)
+        ? apos = limite
+        : apos;
+
+    for (i = apos; i <= limite; i++) {
+
+        fetch(("https://pokeapi.co/api/v2/pokemon/").concat(i))
+            .then((response) => response.json())
+            .then((pokemon) => exibirPokemons(listarPokemon(pokemon)))
+            .catch((erro) => console.log("Erro: " + erro));
+
+    }
+
+}
+
+function listarPokemon(pokemon) {
+    let id = pokemon["id"];
+    let nome = pokemon["name"];
+    let imagem = pokemon["sprites"]["front_default"];
+    let tipos = [];
+    let vida;
+    let ataque;
+    let defesa;
+    let ataqueEspecial;
+    let defesaEspecial;
+    let velocidade;
+
+    if (pokemon["types"] !== undefined) {
+        pokemon["types"].forEach(tipo => {
+            tipos.push(tipo["type"]["name"]);
+        });
+    }
+
+    if (pokemon["stats"] !== undefined) {
+        pokemon["stats"].forEach(estatistica => {
+            switch (estatistica["stat"]["name"]) {
+                case "hp":
+                    vida = [estatistica["base_stat"], estatistica["effort"]];
+                    break;
+                case "attack":
+                    ataque = [estatistica["base_stat"], estatistica["effort"]];
+                    break;
+                case "defense":
+                    defesa = [estatistica["base_stat"], estatistica["effort"]];
+                    break;
+                case "special-attack":
+                    ataqueEspecial = [estatistica["base_stat"], estatistica["effort"]];
+                    break;
+                case "special-defense":
+                    defesaEspecial = [estatistica["base_stat"], estatistica["effort"]];
+                    break;
+                case "speed":
+                    velocidade = [estatistica["base_stat"], estatistica["effort"]];
+                    break;
+            }
+        });
+    }
+
+    let objeto = {
+        id: id,
+        nome: nome,
+        imagem: imagem,
+        tipos: tipos,
+        vida: vida,
+        ataque: ataque,
+        defesa: defesa,
+        ataqueEspecial: ataqueEspecial,
+        defesaEspecial: defesaEspecial,
+        velocidade: velocidade,
+    };
+
+    return objeto;
+}
+
+function exibirPokemons(listaPokemons) {
+    let pokedex = document.getElementById("pokedex-lista");
 
 
-function requisicaoAPI(url, limite, atual) {
+    let liPokemon = document.createElement("li");
+    liPokemon.className = "pokemon";
 
+    let spanIndice = document.createElement("span");
+    spanIndice.className = "indice";
+    spanIndice.textContent = listaPokemons["id"];
 
+    let spanNome = document.createElement("span");
+    spanNome.className = "nome";
+    spanNome.textContent = listaPokemons["nome"];
 
+    let divDetalhes = document.createElement("div");
+    divDetalhes.className = "detalhes";
 
-    fetch( url.concat( atual ) ).then(
-        function (response) {
-            console.log(response.status);
-        }
-    ).catch(
-        function (erro){
-            console.log(erro);
-        }
-    );
+    let olTipos = document.createElement("ol");
+    olTipos.className = "tipos";
 
+    listaPokemons["tipos"].forEach(tipo => {
 
+        let liTipo = document.createElement("li");
+        liTipo.className = "slot-tipos";
 
+        let spanTipo = document.createElement("span");
+        spanTipo.textContent = tipo;
 
+        liTipo.appendChild(spanTipo);
+        olTipos.appendChild(liTipo);
+
+    });
+
+    let imagem = document.createElement("img");
+    imagem.src = listaPokemons["imagem"];
+    imagem.alt = "Imagem do Pokemon";
+
+    divDetalhes.appendChild(olTipos);
+    divDetalhes.appendChild(imagem);
+
+    liPokemon.appendChild(spanIndice);
+    liPokemon.appendChild(spanNome);
+    liPokemon.appendChild(divDetalhes);
+
+    pokedex.appendChild(liPokemon);
 
 }
--- assets/js/pokemon.js
+++
@@ -1,9 +0,0 @@
-class Pokemon {
-    constructor(id, nome, imagem, tipos, estatisticas){
-        this.id = id;
-        this.nome = nome;
-        this.imagem = imagem;
-        this.tipos = tipos;
-        this.estatisticas = estatisticas;
-    }
-}
