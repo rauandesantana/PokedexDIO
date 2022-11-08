eab5baddb45d278750fac5ff994ac78771762dc8
Rauan Rosa <rauanoronha@hotmail.com>
2022-11-08 08:44:38 -0300

    Organizando e Estruturando Barra de Busca

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,61 @@
-const buscar = [
-    document.getElementById(""),
-    document.getElementById(""),
-];
+// Poke API
 const pokedex = document.getElementById("pokedex-lista");
 const pokeAPI = new PokeAPI(pokedex);
+pokeAPI.inicializar();
 
-pokeAPI.inicializar(1, 898);
+// Barra de Busca
+const buscar = [
+    document.getElementById("buscar-barra"),
+    document.getElementById("buscar-flutuante"),
+];
+const btnBuscar = document.getElementById("buscar-icone");
+const inputBusca = document.createElement("input");
+inputBusca.id = "buscar-texto";
+inputBusca.type = "search";
+inputBusca.placeholder = "Buscar...";
+let buscaFlutuante;
+let textoBusca = "";
+
+let mediaQuerry = window.matchMedia("(max-width: 500px)");
+alternarInputBusca(mediaQuerry.matches);
+
+let elementoBusca = document.getElementById("buscar-texto");
+
+btnBuscar.addEventListener("click", () => {
+    ativarBuscaFlutuante(!buscaFlutuante);
+});
+
+mediaQuerry.addEventListener("change", () => {
+    alternarInputBusca(mediaQuerry.matches);
+});
+
+elementoBusca.addEventListener("input", () => {
+    textoBusca = elementoBusca.value;
+});
+
+function alternarInputBusca(valor) {
+    ativarBuscaFlutuante(false);
+
+    if (!valor) {
+        buscar[0].appendChild(inputBusca);
+    } else {
+        buscar[1].appendChild(inputBusca);
+    }
+}
+
+function ativarBuscaFlutuante(valor) {
+
+    (mediaQuerry.matches)
+        ? (textoBusca.length > 0)
+            ? valor = true
+            : valor
+        : valor;
+
+    if (valor && mediaQuerry.matches) {
+        buscar[1].style.display = "block";
+    } else {
+        buscar[1].style.display = "none";
+    }
+
+    buscaFlutuante = valor;
+}
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -9,87 +9,154 @@ class PokeAPI {
         this.listaPokemons = [];
     }
 
-    inicializar(inicial, final) {
-        (final === undefined || final < 1)
-            ? final = 898
-            : (final > 898)
-                ? final = 898
-                : final;
-        (inicial === undefined || inicial < 1)
-            ? inicial = 1
-            : (inicial > final)
-                ? inicial = final
-                : inicial;
+    inicializar(inicial, limite) {
+        (limite === undefined || limite < 1)
+            ? limite = 999999
+            : limite;
 
-        for (let i = inicial; i <= final; i++) {
+        (inicial === undefined || inicial < 0 || inicial > limite)
+            ? inicial = 0
+            : inicial;
 
-            fetch(this.URL.concat(i))
-                .then((response) => response.json())
-                .then((response) => this.gerarPokemon(response))
-                .then((pokemon) => this.listaPokemons.push(pokemon))
-                .then(() => {
-                    if (this.listaPokemons.length === (final - (inicial - 1))) {
-                        const listaOrdenada = this.listaPokemons.sort(
-                            (a, b) => a["indice"] - b["indice"]
-                            );
-                        this.listaPokemons = listaOrdenada;
-                        this.apresentrarListaPokemon(this.listaPokemons);
-                    }
-                })
-                .catch((erro) => console.log("Erro: " + erro));
+        fetch(this.URL.concat("?offset=" + inicial + "&limit=" + limite + ""))
+            .then((response) => response.json())
+            .then((responseJson) => responseJson["results"])
+            .then((resoltado) => resoltado.map(this.listarRequisicoes))
+            .then((listaRequisicao) => Promise.all(listaRequisicao))
+            .then((resultados) => resultados.map(this.instanciarPokemon))
+            .then((listaObjetos) => {
+                this.listaPokemons = listaObjetos;
+                this.apresentrarListaPokemon(listaObjetos);
+            })
+            .catch((erro) => console.log("(Estagio 1) Erro: " + erro));
+    }
 
+    async listarRequisicoes(item) {
+        try {
+            const response = await fetch(item["url"]);
+            return await response.json();
+        } catch (erro) {
+            return console.log("(Estagio 2) Erro: " + erro);
         }
     }
 
-    gerarPokemon(pokemon) {
-        let indice = pokemon["id"];
-        let nome = pokemon["name"];
-        let imagem = pokemon["sprites"]["front_default"];
-        let tipos = [];
-        let vida;
-        let ataque;
-        let defesa;
-        let ataqueEspecial;
-        let defesaEspecial;
-        let velocidade;
+    instanciarPokemon(pokemon) {
+        let indice, nome, imagem;
+        let tipos, altura, peso;
+        let vida, ataque, defesa;
+        let ataqueEspecial, defesaEspecial, velocidade;
 
+        // Indice
+        (pokemon["id"] === undefined || pokemon["id"] === null)
+            ? indice = "Null"
+            : indice = pokemon["id"];
 
-        if (pokemon["types"] !== undefined) {
+        // Nome
+        (pokemon["name"] === undefined || pokemon["name"] === null)
+            ? nome = "Null"
+            : nome = pokemon["name"];
+
+        // Imagem
+        (pokemon["sprites"]["front_default"] === undefined || pokemon["sprites"]["front_default"] === null)
+            ? imagem = "./assets/imagens/pokeball.png"
+            : imagem = pokemon["sprites"]["front_default"];
+
+        // Tipos
+        if (pokemon["types"] !== undefined || pokemon["types"] === null) {
+            tipos = [];
             pokemon["types"].forEach(tipo => {
-                tipos.push(tipo["type"]["name"]);
+                let valorTipo;
+
+                (tipo["type"]["name"] === undefined || tipo["type"]["name"] === null)
+                    ? valorTipo = "Null"
+                    : valorTipo = tipo["type"]["name"];
+
+                tipos.push(valorTipo);
             });
+        } else {
+            tipos = ["Null"];
         }
 
-        if (pokemon["stats"] !== undefined) {
+        // Altura
+        (pokemon["height"] === undefined || pokemon["height"] === null)
+            ? altura = "Null"
+            : altura = (pokemon["height"] / 10);
+
+        // Peso
+        (pokemon["weight"] === undefined || pokemon["weight"] === null)
+            ? peso = "Null"
+            : peso = pokemon["weight"];
+
+        // Estatisticas
+        if (pokemon["stats"] !== undefined || pokemon["stats"] === null) {
             pokemon["stats"].forEach(estatistica => {
-                switch (estatistica["stat"]["name"]) {
+                let nomeAtributo = estatistica["stat"]["name"];
+                let valorBase;
+                let valorAdd;
+
+                (estatistica["base_stat"] === undefined || pokemon["base_stat"] === null)
+                    ? valorBase = "Null"
+                    : valorBase = estatistica["base_stat"];
+
+                (estatistica["effort"] === undefined || pokemon["effort"] === null)
+                    ? valorAdd = "Null"
+                    : valorAdd = estatistica["effort"];
+
+                switch (nomeAtributo) {
                     case "hp":
-                        vida = [estatistica["base_stat"], estatistica["effort"]];
+                        vida = [valorBase, valorAdd];
                         break;
                     case "attack":
-                        ataque = [estatistica["base_stat"], estatistica["effort"]];
+                        ataque = [valorBase, valorAdd];
                         break;
                     case "defense":
-                        defesa = [estatistica["base_stat"], estatistica["effort"]];
+                        defesa = [valorBase, valorAdd];
                         break;
                     case "special-attack":
-                        ataqueEspecial = [estatistica["base_stat"], estatistica["effort"]];
+                        ataqueEspecial = [valorBase, valorAdd];
                         break;
                     case "special-defense":
-                        defesaEspecial = [estatistica["base_stat"], estatistica["effort"]];
+                        defesaEspecial = [valorBase, valorAdd];
                         break;
                     case "speed":
-                        velocidade = [estatistica["base_stat"], estatistica["effort"]];
+                        velocidade = [valorBase, valorAdd];
                         break;
                 }
             });
         }
 
+        (vida === undefined)
+            ? vida = ["Null", "Null"]
+            : vida;
+
+        (ataque === undefined)
+            ? ataque = ["Null", "Null"]
+            : ataque;
+
+        (defesa === undefined)
+            ? defesa = ["Null", "Null"]
+            : defesa;
+
+        (ataqueEspecial === undefined)
+            ? ataqueEspecial = ["Null", "Null"]
+            : ataqueEspecial;
+
+        (defesaEspecial === undefined)
+            ? defesaEspecial = ["Null", "Null"]
+            : defesaEspecial;
+
+        (velocidade === undefined)
+            ? velocidade = ["Null", "Null"]
+            : velocidade;
+
+        // Atribuir Objeto
         let objeto = {
             indice: indice,
             nome: nome,
             imagem: imagem,
             tipos: tipos,
+            altura: altura,
+            peso: peso,
             vida: vida,
             ataque: ataque,
             defesa: defesa,
@@ -103,50 +170,48 @@ class PokeAPI {
 
     apresentrarListaPokemon(listaPokemons) {
         listaPokemons.forEach(pokemon => {
-
             let liPokemon = document.createElement("li");
             liPokemon.className = "pokemon";
-    
+
             let spanIndice = document.createElement("span");
             spanIndice.className = "indice";
-            spanIndice.textContent = pokemon["indice"];
-    
+            spanIndice.textContent = "#" + pokemon["indice"];
+
             let spanNome = document.createElement("span");
             spanNome.className = "nome";
             spanNome.textContent = pokemon["nome"];
-    
+
             let divDetalhes = document.createElement("div");
             divDetalhes.className = "detalhes";
-    
+
             let olTipos = document.createElement("ol");
             olTipos.className = "tipos";
-    
+
             pokemon["tipos"].forEach(tipo => {
-    
+
                 let liTipo = document.createElement("li");
                 liTipo.className = "slot-tipos";
-    
+
                 let spanTipo = document.createElement("span");
                 spanTipo.textContent = tipo;
-    
+
                 liTipo.appendChild(spanTipo);
                 olTipos.appendChild(liTipo);
-    
+
             });
-    
+
             let imagem = document.createElement("img");
             imagem.src = pokemon["imagem"];
             imagem.alt = "Imagem do Pokemon";
-    
+
             divDetalhes.appendChild(olTipos);
             divDetalhes.appendChild(imagem);
-    
+
             liPokemon.appendChild(spanIndice);
             liPokemon.appendChild(spanNome);
             liPokemon.appendChild(divDetalhes);
-    
-            this.pokedex.appendChild(liPokemon);
 
+            this.pokedex.appendChild(liPokemon);
         });
     }
 }
